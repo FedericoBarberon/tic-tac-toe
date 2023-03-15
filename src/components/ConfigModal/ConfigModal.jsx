ec6597734d8cfffb016da7412e64c7689311126b
Federico Barberon <jfedericobarberonj@gmail.com>
2023-03-15 07:27:50 +0000

    Add player name config

--- src/components/ConfigModal/ConfigModal.jsx
+++ src/components/ConfigModal/ConfigModal.jsx
@@ -5,7 +5,7 @@ import './configModal.scss'
 import Close from '../Close/Close'
 
 export default function ConfigModal ({ handleClose }) {
-  const { avatars, changeAvatars } = useConfig()
+  const { avatars, changeAvatars, multiplayerName, changeMultiplayerName } = useConfig()
 
   const handleAvatarChange = (event) => {
     event.preventDefault()
@@ -13,7 +13,10 @@ export default function ConfigModal ({ handleClose }) {
     const p1 = event.target.p1.value
     const p2 = event.target.p2.value
 
-    changeAvatars({ p1, p2 })
+    const playerName = event.target.playerName.value
+
+    if (p1 !== avatars.p1 || p2 !== avatars.p2) changeAvatars({ p1, p2 })
+    if (playerName !== multiplayerName) changeMultiplayerName(playerName)
     handleClose()
   }
 
@@ -25,16 +28,26 @@ export default function ConfigModal ({ handleClose }) {
         <Close handleClose={handleClose} />
       </header>
 
-      <form className='players-form' onSubmit={handleAvatarChange}>
-        <h3>Avatars</h3>
-        <label>
-          Player 1
-          <input defaultValue={avatars.p1} name='p1' minLength={1} maxLength={2} placeholder='X' required />
-        </label>
-        <label>
-          Player 2
-          <input defaultValue={avatars.p2} name='p2' minLength={1} maxLength={2} placeholder='O' required />
-        </label>
+      <form className='config-form' onSubmit={handleAvatarChange}>
+        <div className='form-group'>
+          <h3>Avatars</h3>
+          <label className='avatar-input-label'>
+            Player 1
+            <input defaultValue={avatars.p1} name='p1' minLength={1} maxLength={2} placeholder='X' required />
+          </label>
+          <label className='avatar-input-label'>
+            Player 2
+            <input defaultValue={avatars.p2} name='p2' minLength={1} maxLength={2} placeholder='O' required />
+          </label>
+        </div>
+
+        <div className='form-group'>
+          <h3>Multiplayer</h3>
+          <label className='multiplayerName-input-label'>
+            Player Name
+            <input defaultValue={multiplayerName} name='playerName' minLength={3} maxLength={20} placeholder='Enter your player name' required />
+          </label>
+        </div>
 
         <button className='button'>Save</button>
       </form>
--- src/context/ConfigContext.jsx
+++ src/context/ConfigContext.jsx
@@ -4,12 +4,14 @@ import { useLocalStorage } from '../hooks/useLocalStorage'
 export const ConfigContext = createContext()
 
 export default function ConfigProvider ({ children }) {
+  const [multiplayerName, setMultiplayerName] = useLocalStorage('multiplayerName', 'player unknown')
   const [avatars, setAvatars] = useLocalStorage('avatars', { p1: '❌', p2: '🔵' })
 
+  const changeMultiplayerName = (newName) => setMultiplayerName(newName)
   const changeAvatars = (newAvatars) => setAvatars(newAvatars)
 
   return (
-    <ConfigContext.Provider value={{ avatars, changeAvatars }}>
+    <ConfigContext.Provider value={{ avatars, changeAvatars, multiplayerName, changeMultiplayerName }}>
       {children}
     </ConfigContext.Provider>
   )
